5eae655b347fafebec64297c803146311ec6e65f
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2019-11-25 13:58:06 +0900

    phone call
    call button

--- MyApplication7/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication7/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button newpage = (Button)findViewById(R.id.bt1);
+        newpage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "다음 페이지입니다", Toast.LENGTH_LONG).show();
+                Intent myintent = new Intent(MainActivity.this,Main2Activity.class);
+                myintent.putExtra("name","와이프");
+                myintent.putExtra("age",23);
+                startActivity(myintent);
+
+            }
+        });
+
+        Button call = (Button)findViewById(R.id.bt1I);
+        call.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                Toast.makeText(getApplicationContext(),"전화가 걸립니다", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:/01012345678"));
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
