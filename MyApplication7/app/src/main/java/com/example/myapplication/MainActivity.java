c1614687fc641692b235e832762a88cb38cfe36c
juuyoungjeon <44489852+juuyoungjeon@users.noreply.github.com>
2019-11-28 09:51:44 +0900

    Error fix
    two on create functions >> Fix include in

--- MyApplication7/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication7/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -28,20 +28,55 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    private GpsTracker gpsTracker;
-
-    private static final int GPS_ENABLE_REQUEST_CODE = 2001;
-    private static final int PERMISSIONS_REQUEST_CODE = 100;
-    String[] REQUIRED_PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button newpage = (Button) findViewById(R.id.bt1);
+        newpage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "다음 페이지입니다", Toast.LENGTH_LONG).show();
+                Intent myintent = new Intent(MainActivity.this, Main2Activity.class);
+                myintent.putExtra("name", "와이프");
+                myintent.putExtra("age", 23);
+                startActivity(myintent);
 
+            }
+        });
+
+        Button call = (Button) findViewById(R.id.bt1I);
+        call.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "전화가 걸립니다", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:/01012345678"));
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        Button call2 = (Button) findViewById(R.id.bt2I);
+        call2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "전화가 걸립니다", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:/01011111111"));
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        Button call3 = (Button) findViewById(R.id.camera);
+        call3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "카메라로 연결됩니다", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent();
+                intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(intent);
+                finish();
+            }
+        });
         if (!checkLocationServicesStatus()) {
 
             showDialogForLocationServiceSetting();
@@ -70,6 +105,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private GpsTracker gpsTracker;
+
+    private static final int GPS_ENABLE_REQUEST_CODE = 2001;
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
+    String[] REQUIRED_PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+
+
+
 
 
     /*
